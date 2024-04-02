1e0141cc4a76478b90a8afa2263abf13d4aa482e
jetty96 <125943122+jetty96@users.noreply.github.com>
2024-04-02 09:14:27 -0400

    Add files via upload

---
+++ my-project 2/src/main/java/com/example/workflow/CamundaConfig.java
@@ -0,0 +1,23 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamundaConfig {
+
+//    @Bean
+//    public RuntimeService runtimeService() {
+//        return ProcessEngines.getDefaultProcessEngine().getRuntimeService();
+//    }
+//
+//    @Bean
+//    public TaskService taskService() {
+//        return ProcessEngines.getDefaultProcessEngine().getTaskService();
+//    }
+
+
+}
